3b69e3bdaf4a162f66d4b582daecde72869819c8
Shlomi Nugarker <shlomin1231@gmail.com>
2022-11-09 19:32:42 +0200

    added mediaQuery

--- src/cmps/Header.tsx
+++ src/cmps/Header.tsx
@@ -1,27 +1,41 @@
+import { useState } from 'react'
+
 export const Header = (props: any) => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false)
+
   return (
     <header className="header-cmp">
-      <div>
-        <span className="logo">lichess.com</span>
-        <ul>
-          <li>Play</li>
+      <div
+        className={` ${!isMenuVisible ? 'container hide-menu' : 'container'}`}
+      >
+        <span className="logo">chess logo</span>
+        <div className="nav">
+          <ul>
+            <li>Play</li>
 
-          <li>Puzzles</li>
+            <li>Puzzles</li>
 
-          <li>Learn</li>
+            <li>Learn</li>
 
-          <li>Watch</li>
+            <li>Watch</li>
 
-          <li>Community</li>
+            <li>Community</li>
 
-          <li>Tools</li>
-        </ul>
-      </div>
-
-      <div>
-        <div className="sign-in">
-          <p>Sign In</p>
+            <li>Tools</li>
+          </ul>
         </div>
+
+        <div className="sign-in-container">
+          <div className="sign-in">
+            <p>Sign In</p>
+          </div>
+        </div>
+      </div>
+      <div
+        className="menu-btn hide"
+        onClick={() => setIsMenuVisible(!isMenuVisible)}
+      >
+        x
       </div>
     </header>
   )
