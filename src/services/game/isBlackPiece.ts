66f1e3d828e113cfe6fac20d1c354f486c8a97e7
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-12 01:21:53 +0300

    reactor isBlackPiece func

--- src/services/game/isBlackPiece.ts
+++ src/services/game/isBlackPiece.ts
@@ -6,38 +6,18 @@ export function isBlackPiece(
 ): boolean | undefined {
   switch (piece) {
     case state.pieces.KING_WHITE:
-      return false
-
     case state.pieces.BISHOP_WHITE:
-      return false
-
     case state.pieces.PAWN_WHITE:
-      return false
-
     case state.pieces.QUEEN_WHITE:
-      return false
-
     case state.pieces.ROOK_WHITE:
-      return false
-
     case state.pieces.KNIGHT_WHITE:
       return false
 
     case state.pieces.KING_BLACK:
-      return true
-
     case state.pieces.BISHOP_BLACK:
-      return true
-
     case state.pieces.PAWN_BLACK:
-      return true
-
     case state.pieces.QUEEN_BLACK:
-      return true
-
     case state.pieces.ROOK_BLACK:
-      return true
-
     case state.pieces.KNIGHT_BLACK:
       return true
 
