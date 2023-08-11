266f076350442b4dc51a388f28df839363a5b091
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-12 00:42:29 +0300

    improve getAllPossibleCoordsBishop func

--- src/services/game/possibleCoordsFuncs/getAllPossibleCoordsBishop.ts
+++ src/services/game/possibleCoordsFuncs/getAllPossibleCoordsBishop.ts
@@ -4,31 +4,29 @@ import { isEmptyCell } from '../isEmptyCell'
 
 export function getAllPossibleCoordsBishop(
   state: GameState,
-  pieceCoord: {
-    i: number
-    j: number
-  }
+  pieceCoord: { i: number; j: number }
 ) {
   const { board } = state
 
-  let res: { i: number; j: number }[] = []
-
-  const possibleDir = [
-    { i: 1, j: -1 }, //bottomLeft
-    { i: 1, j: 1 }, //bottomRight
-    { i: -1, j: -1 }, //topLeft
-    { i: -1, j: 1 }, //topRight
+  const possibleDirections = [
+    { i: 1, j: -1 }, // bottomLeft
+    { i: 1, j: 1 }, // bottomRight
+    { i: -1, j: -1 }, // topLeft
+    { i: -1, j: 1 }, // topRight
   ]
 
-  for (let k = 0; k < possibleDir.length; k++) {
-    for (let i = 1; i <= 8; i++) {
-      const diffI = i * possibleDir[k].i
-      const diffJ = i * possibleDir[k].j
+  const possibleCoords: { i: number; j: number }[] = []
+
+  for (const direction of possibleDirections) {
+    for (let distance = 1; distance <= 8; distance++) {
+      const diffI = distance * direction.i
+      const diffJ = distance * direction.j
 
       const nextCoord = {
         i: pieceCoord.i + diffI,
         j: pieceCoord.j - diffJ,
       }
+
       if (
         nextCoord.i > 7 ||
         nextCoord.i < 0 ||
@@ -39,13 +37,16 @@ export function getAllPossibleCoordsBishop(
       }
 
       if (isEmptyCell(board, nextCoord)) {
-        res.push(nextCoord)
+        possibleCoords.push(nextCoord)
       } else {
         const piece = board[nextCoord.i][nextCoord.j]
-        if (!isColorPieceWorthCurrPlayerColor(state, piece)) res.push(nextCoord) //last coord -> eatable
+        if (!isColorPieceWorthCurrPlayerColor(state, piece)) {
+          possibleCoords.push(nextCoord) // last coord -> eatable
+        }
         break
       }
     }
   }
-  return res
+
+  return possibleCoords
 }
