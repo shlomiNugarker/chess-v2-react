792d4fe2a19a8861c07b17f01f33a929863662b0
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-12 14:48:36 +0300

    add test - buildBoard ()

---
+++ src/services/game/__tests___/BuildBoard.test.ts
@@ -0,0 +1,59 @@
+import { buildBoard } from '../buildBoard'
+import { gPieces } from '../gPieces'
+
+describe('buildBoard', () => {
+  test('builds the initial chess board correctly', () => {
+    const pieces = gPieces
+
+    const expectedBoard: string[][] = [
+      [
+        pieces.ROOK_BLACK,
+        pieces.KNIGHT_BLACK,
+        pieces.BISHOP_BLACK,
+        pieces.QUEEN_BLACK,
+        pieces.KING_BLACK,
+        pieces.BISHOP_BLACK,
+        pieces.KNIGHT_BLACK,
+        pieces.ROOK_BLACK,
+      ],
+      [
+        pieces.PAWN_BLACK,
+        pieces.PAWN_BLACK,
+        pieces.PAWN_BLACK,
+        pieces.PAWN_BLACK,
+        pieces.PAWN_BLACK,
+        pieces.PAWN_BLACK,
+        pieces.PAWN_BLACK,
+        pieces.PAWN_BLACK,
+      ],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      [
+        pieces.PAWN_WHITE,
+        pieces.PAWN_WHITE,
+        pieces.PAWN_WHITE,
+        pieces.PAWN_WHITE,
+        pieces.PAWN_WHITE,
+        pieces.PAWN_WHITE,
+        pieces.PAWN_WHITE,
+        pieces.PAWN_WHITE,
+      ],
+      [
+        pieces.ROOK_WHITE,
+        pieces.KNIGHT_WHITE,
+        pieces.BISHOP_WHITE,
+        pieces.QUEEN_WHITE,
+        pieces.KING_WHITE,
+        pieces.BISHOP_WHITE,
+        pieces.KNIGHT_WHITE,
+        pieces.ROOK_WHITE,
+      ],
+    ]
+
+    const result = buildBoard(pieces)
+
+    expect(result).toEqual(expectedBoard)
+  })
+})
