f1ee12285311fec789b0fe4a653e85266b8d2402
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-12 16:02:12 +0300

    test - getAllPossibleCoordsQueen()

---
+++ src/services/game/__tests___/possibleCoordsFuncsTests/getAllPossibleCoordsQueen.test.ts
@@ -0,0 +1,157 @@
+import { getAllPossibleCoordsQueen } from '../../possibleCoordsFuncs/getAllPossibleCoordsQueen'
+import { gPieces } from '../../gPieces'
+import { GameState } from '../../../../models/GameState'
+
+describe('getAllPossibleCoordsQueen', () => {
+  test('returns possible coordinates for a queen with obstructions', () => {
+    const pieces = gPieces
+
+    const state = {
+      pieces,
+      isBlackTurn: true,
+      isCastlingLegal: { blackKing: true, whiteKing: true },
+      board: [
+        [
+          pieces.ROOK_BLACK,
+          pieces.KNIGHT_BLACK,
+          pieces.BISHOP_BLACK,
+          pieces.QUEEN_BLACK,
+          pieces.KING_BLACK,
+          pieces.BISHOP_BLACK,
+          pieces.KNIGHT_BLACK,
+          pieces.ROOK_BLACK,
+        ],
+        [
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+        ],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        [
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+        ],
+        [
+          pieces.ROOK_WHITE,
+          pieces.KNIGHT_WHITE,
+          pieces.BISHOP_WHITE,
+          pieces.QUEEN_WHITE,
+          pieces.KING_WHITE,
+          pieces.BISHOP_WHITE,
+          pieces.KNIGHT_WHITE,
+          pieces.ROOK_WHITE,
+        ],
+      ],
+    }
+
+    const pieceCoord = { i: 0, j: 3 }
+    const result = getAllPossibleCoordsQueen(state as GameState, pieceCoord)
+
+    const expectedCoords: {
+      i: number
+      j: number
+    }[] = []
+
+    expect(result).toEqual(expectedCoords)
+  })
+
+  test('returns possible coordinates for a queen with no obstructions', () => {
+    const pieces = gPieces
+
+    const state = {
+      pieces,
+      isBlackTurn: true,
+      isCastlingLegal: { blackKing: true, whiteKing: true },
+      board: [
+        [
+          pieces.ROOK_BLACK,
+          pieces.KNIGHT_BLACK,
+          pieces.BISHOP_BLACK,
+          '',
+          pieces.KING_BLACK,
+          pieces.BISHOP_BLACK,
+          pieces.KNIGHT_BLACK,
+          pieces.ROOK_BLACK,
+        ],
+        [
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+        ],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', pieces.QUEEN_BLACK, '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        [
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+        ],
+        [
+          pieces.ROOK_WHITE,
+          pieces.KNIGHT_WHITE,
+          pieces.BISHOP_WHITE,
+          pieces.QUEEN_WHITE,
+          pieces.KING_WHITE,
+          pieces.BISHOP_WHITE,
+          pieces.KNIGHT_WHITE,
+          pieces.ROOK_WHITE,
+        ],
+      ],
+    }
+
+    const pieceCoord = { i: 4, j: 3 }
+    const result = getAllPossibleCoordsQueen(state as GameState, pieceCoord)
+
+    const expectedCoords: {
+      i: number
+      j: number
+    }[] = [
+      { i: 5, j: 4 },
+      { i: 6, j: 5 },
+      { i: 5, j: 2 },
+      { i: 6, j: 1 },
+      { i: 3, j: 4 },
+      { i: 2, j: 5 },
+      { i: 3, j: 2 },
+      { i: 2, j: 1 },
+      { i: 3, j: 3 },
+      { i: 2, j: 3 },
+      { i: 5, j: 3 },
+      { i: 6, j: 3 },
+      { i: 4, j: 2 },
+      { i: 4, j: 1 },
+      { i: 4, j: 0 },
+      { i: 4, j: 4 },
+      { i: 4, j: 5 },
+      { i: 4, j: 6 },
+      { i: 4, j: 7 },
+    ]
+
+    expect(result).toEqual(expectedCoords)
+  })
+})
