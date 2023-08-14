27f2a20029c06fc5717d359a038fab0f4cb360e1
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-14 12:02:16 +0300

    test - isPawnStepsEnd()

---
+++ src/services/game/__tests___/isPawnStepsEnd.test.ts
@@ -0,0 +1,123 @@
+import { isPawnStepsEnd } from '../isPawnStepsEnd'
+import { gPieces } from '../gPieces'
+import { GameState } from '../../../models/GameState'
+
+describe('isPawnStepsEnd', () => {
+  test('returns false for black pawn at the board', () => {
+    const pieces = gPieces
+    const state = {
+      board: [
+        [
+          pieces.ROOK_BLACK,
+          pieces.KNIGHT_BLACK,
+          pieces.BISHOP_BLACK,
+          pieces.QUEEN_BLACK,
+          pieces.KING_BLACK,
+          pieces.BISHOP_BLACK,
+          pieces.KNIGHT_BLACK,
+          pieces.ROOK_BLACK,
+        ],
+        [
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+        ],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', pieces.QUEEN_WHITE, '', '', ''],
+        [
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+        ],
+        [
+          pieces.ROOK_WHITE,
+          pieces.KNIGHT_WHITE,
+          pieces.BISHOP_WHITE,
+          '',
+          pieces.KING_WHITE,
+          pieces.BISHOP_WHITE,
+          pieces.KNIGHT_WHITE,
+          pieces.ROOK_WHITE,
+        ],
+      ],
+      pieces,
+      isBlackTurn: true,
+    }
+    const coord = { i: 6, j: 0 }
+
+    const result = isPawnStepsEnd(state as GameState, coord)
+
+    expect(result).toBe(false)
+  })
+
+  test('returns true for white pawn at the end of the board', () => {
+    const pieces = gPieces
+    const state = {
+      board: [
+        [
+          pieces.PAWN_WHITE,
+          pieces.KNIGHT_BLACK,
+          pieces.BISHOP_BLACK,
+          pieces.QUEEN_BLACK,
+          pieces.KING_BLACK,
+          pieces.BISHOP_BLACK,
+          pieces.KNIGHT_BLACK,
+          pieces.ROOK_BLACK,
+        ],
+        [
+          '',
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+          pieces.PAWN_BLACK,
+        ],
+        ['', '', '', '', '', '', '', pieces.PAWN_BLACK],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', pieces.QUEEN_WHITE, '', '', ''],
+        [
+          pieces.PAWN_WHITE,
+          '',
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+          pieces.PAWN_WHITE,
+        ],
+        [
+          pieces.ROOK_WHITE,
+          pieces.KNIGHT_WHITE,
+          pieces.BISHOP_WHITE,
+          '',
+          pieces.KING_WHITE,
+          pieces.BISHOP_WHITE,
+          pieces.KNIGHT_WHITE,
+          pieces.ROOK_WHITE,
+        ],
+      ],
+      pieces,
+      isBlackTurn: false,
+    }
+    const coord = { i: 0, j: 0 }
+
+    const result = isPawnStepsEnd(state as GameState, coord)
+
+    expect(result).toBe(true)
+  })
+})
--- src/services/game/isPawnStepsEnd.ts
+++ src/services/game/isPawnStepsEnd.ts
@@ -14,4 +14,5 @@ export function isPawnStepsEnd(
   ) {
     return true
   }
+  return false
 }
