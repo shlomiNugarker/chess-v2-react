015f142d8341ce2fe58e08e63c2214ccd51368f2
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-17 01:35:47 +0300

    improve isPlayerWin()

--- src/services/game/isPlayerWin.ts
+++ src/services/game/isPlayerWin.ts
@@ -6,61 +6,36 @@ import _ from 'lodash'
 
 export function isPlayerWin(state: GameState) {
   const { board, isBlackTurn } = state
+  let color = isBlackTurn ? 'black' : 'white'
+  let isWin = true
 
-  if (isBlackTurn) {
-    let isBlackWin = true
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        const piece = board[i][j]
-        if (isBlackPiece(state, piece)) {
-          const possibleCoords = getPossibleCoords(state, piece, { i, j })
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const piece = board[i][j]
+      if (
+        color === 'black'
+          ? isBlackPiece(state, piece)
+          : !isBlackPiece(state, piece) &&
+            typeof isBlackPiece(state, piece) === 'boolean'
+      ) {
+        const possibleCoords = getPossibleCoords(state, piece, { i, j })
 
-          if (!possibleCoords) return
-          for (let k = 0; k < possibleCoords.length; k++) {
-            const coord = possibleCoords[k]
-            const copyState = _.cloneDeep(state)
-            copyState.selectedCellCoord = { i, j }
-            const element = {} as Element
-            element.id = `cell-${coord.i}-${coord.j}`
-            const { isMoveLegal } = isNextStepLegal(copyState, element)
+        if (!possibleCoords) return
+        for (let k = 0; k < possibleCoords.length; k++) {
+          const coord = possibleCoords[k]
+          const copyState = _.cloneDeep(state)
+          copyState.selectedCellCoord = { i, j }
+          const element = {} as Element
+          element.id = `cell-${coord.i}-${coord.j}`
+          const { isMoveLegal } = isNextStepLegal(copyState, element)
 
-            if (isMoveLegal) {
-              isBlackWin = false
-              break
-            }
+          if (isMoveLegal) {
+            isWin = false
+            break
           }
         }
       }
     }
-
-    return isBlackWin
-  } else if (!isBlackTurn) {
-    let isWhiteWin = true
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        const piece = board[i][j]
-        const isBlack = isBlackPiece(state, piece)
-        const isWhite = typeof isBlack === 'boolean' && isBlack === false
-        if (isWhite) {
-          const possibleCoords = getPossibleCoords(state, piece, { i, j })
-
-          if (!possibleCoords) return
-          for (let k = 0; k < possibleCoords.length; k++) {
-            const coord = possibleCoords[k]
-            const copyState = _.cloneDeep(state)
-            copyState.selectedCellCoord = { i, j }
-            const element = {} as Element
-            element.id = `cell-${coord.i}-${coord.j}`
-            const { isMoveLegal } = isNextStepLegal(copyState, element)
-
-            if (isMoveLegal) {
-              isWhiteWin = false
-              break
-            }
-          }
-        }
-      }
-    }
-    return isWhiteWin
   }
+  return isWin
 }
