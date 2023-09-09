1b2b33f5867fb68c0519cc10dad9e9982abd5e8e
Shlomi Nugarker <shlomin1231@gmail.com>
2023-09-09 04:19:27 +0300

    cancel option to add history board (slow)

--- src/services/game/controller/updateGameState.ts
+++ src/services/game/controller/updateGameState.ts
@@ -1,5 +1,4 @@
-import _ from 'lodash'
-import { GameState } from '../../../models/GameState'
+slow)import { GameState } from '../../../models/GameState'
 import { gameStateService } from '../../gameStateService'
 import { socketService } from '../../socketService'
 import { storageService } from '../../storageService'
@@ -9,16 +8,11 @@ export const updateGameState: UpdateGameState = async (
   newState: GameState,
   setGameState: React.Dispatch<React.SetStateAction<GameState | null>>
 ) => {
-  const stateWithBoardHistory = _.cloneDeep(newState)
-
-  if (newState?.board) stateWithBoardHistory.boardHistory.push(newState.board)
-
-  if (!stateWithBoardHistory.isOnline) {
-    storageService.put('chess-game', stateWithBoardHistory)
-
-    setGameState(stateWithBoardHistory)
+  if (!newState.isOnline) {
+    storageService.put('chess-game', newState)
+    setGameState(newState)
   } else {
-    const savedState = await gameStateService.saveState(stateWithBoardHistory)
+    const savedState = await gameStateService.saveState(newState)
     socketService.emit('state-updated', savedState)
     setGameState((prev) => ({
       ...prev,
